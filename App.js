bca18c4d85c6e46bd60cc13fc49c2aeb68eb1e2f
ushareddyyy <158132529+ushareddyyy@users.noreply.github.com>
2025-02-03 13:00:12 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,13 +1,44 @@
 
+import React from "react";
+/* import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      
-      <h1 style={{color:"red"}}>Welcome to reactjs</h1>
+  //let ele=React.createElement("div",{className:"App"},
+  //React.createElement("h1",null,"hello"))
+  let username="usha";
+  return ( */
+  
+    //ele
+    /* <section>
+      <h1>hello</h1>
+      <p>usha</p>
+    </section>  */
+
+    
+/*     <div>
+      <h1>{username}</h1>
+      <p>{1760+2870}</p>
+
     </div>
-  );
+     );
 }
 
+export default App; */
+
+/* class App extends React.Children{
+  render(){
+    return(
+      <h1>finally CBS</h1>
+    )
+  }
+  
+}export default App; */
+
+const App=()=>{
+  return(
+      <h1>hello</h1>
+  )
+
+}
 export default App;
